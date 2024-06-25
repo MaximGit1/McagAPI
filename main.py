6966ab2bd5a693443affe59e340194fa5d529ae1
MaximGit1 <maxt3tch@gmail.com>
2024-06-25 18:58:46 +0300

    add redirect func to docs del pydantic version

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from api_v1 import router as router_v1
 from core.config import settings
 
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # async with db_helper.engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.create_all)
+    print(app, 'has been started')
     yield
 
 
 app = FastAPI(title='@MaximGit1', lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+
+@app.get('/', response_class=RedirectResponse, status_code=302)
+async def home():
+    return '/docs'
 
 if __name__ == '__main__':
     import uvicorn
